57564b98a22da0db37db1d5d779cb7153c37dbe5
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-10-14 11:32:24 +0200

    JUnit Assertions - Equals/NotEqual and Null/NotNull - Overview

---
+++ src/main/java/dev/frizio/demo/DemoUtils.java
@@ -0,0 +1,16 @@
+package dev.frizio.demo;
+
+public class DemoUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public Object checkNull(Object obj) {
+        if (obj != null) {
+            return obj;
+        }
+        return null;
+    }
+
+}
