d7f7cf81fc75828da4f95253f93d091588218fd3
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-10-14 11:39:09 +0200

    Test Case Lifecycle Methods

--- src/test/java/dev/frizio/demo/DemoUtilsTest.java
+++ src/test/java/dev/frizio/demo/DemoUtilsTest.java
@@ -1,25 +1,56 @@
 package dev.frizio.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("Running @BeforeEach");
+    }
+
+    @AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Running @AfterEach");
+    }
+
+    @BeforeAll
+    static void setupBeforeEachClass() {
+        System.out.println("@BeforeAll executes only once before all test methods execution in the class");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll() {
+        System.out.println("@AfterAll executes only once after all test methods execution in the class");
+    }
+
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
+
+        System.out.println("Running test: testEqualsAndNotEquals");
+
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
     }
 
+
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
+
+        System.out.println("Running test: testNullAndNotNull");
+
         String str1 = null;
         String str2 = "luv2code";
+
         assertNull(demoUtils.checkNull(str1), "Object should be null");
         assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+
     }
 
 }
